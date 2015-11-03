abec2ba4a3b48c5ff4308e53c44e87ffcf284108
InnaTsumbalyk <inna_tsumbaluk@mail.ru>
2015-11-03 23:14:05 +0200

    backend for categories

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get 'about' => 'pages#about', as: :about
   get 'contact' => 'pages#contact', as: :contact
 
+  namespace :backend do
+    resources :categories
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
   # See how all your routes lay out with "rake routes".
