f6bbe27b34e92766102fbf0e4f20e1915f3a510d
InnaTsumbalyk <inna_tsumbaluk@mail.ru>
2015-12-02 23:17:00 +0200

    register category as product_categories in activeadmin

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,21 +1,5 @@
-ActiveAdmin.register Category do
+ActiveAdmin.register Category, as: 'Product Categories' do
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-permit_params :title, :short_description, :description, :position,
-              :meta_title, :meta_description, :meta_keywords, :image
-def index
-  collection.page(params[:page]).per(10)
-end
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
-
+  permit_params :title, :short_description, :description, :position, :meta_title, :meta_description, :meta_keywords, :image
 
 end
